5f5a5d4c7ee367451ec73a0e60fe67bb54efb6b5
Bastian Gruber <foreach@me.com>
2021-02-19 16:09:09 +0100

    add chapter 03

---
+++ ch_03/src/main.rs
@@ -0,0 +1,46 @@
+use std::str::FromStr;
+use std::io::{Error, ErrorKind};
+
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
+
+fn main() {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!("faq".to_string())),
+    );
+    println!("{:?}", question);
+}
--- ch_05/src/types/question.rs
+++ ch_05/src/types/question.rs
@@ -4,7 +4,7 @@ pub struct Question {
     pub id: QuestionId,
     pub title: String,
     pub content: String,
-    pub ags: Option<Vec<String>>,
+    pub tags: Option<Vec<String>>,
 }
 #[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
 pub struct QuestionId(pub String);
