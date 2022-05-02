8715c7b73e1e9cfe70cca732da3a4b6e4a899944
Bastian Gruber <code@recv.online>
2022-05-02 10:46:55 +0200

    Update code example

--- ch_05/src/types/pagination.rs
+++ ch_05/src/types/pagination.rs
@@ -19,31 +19,34 @@ pub struct Pagination {
 /// `/questions?start=1&end=10`
 /// # Example usage
 /// ```rust
-/// let query = HashMap::new();
-/// query.push("start", "1");
-/// query.push("end", "10");
+/// let mut query = HashMap::new();
+/// query.push("start".to_string(), "1".to_string());
+/// query.push("end".to_string(), "10".to_string());
 /// let p = types::pagination::extract_pagination(query).unwrap();
 /// assert_eq!(p.start, 1);
 /// assert_eq!(p.end, 10);
 /// ```
-pub fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, Error> {
-    // Could be improved in the future
-    if params.contains_key("start") && params.contains_key("end") {
-        return Ok(Pagination {
-            // Takes the "start" parameter in the query and tries to convert it to a number
-            start: params
-                .get("start")
-                .unwrap()
-                .parse::<usize>()
-                .map_err(Error::ParseError)?,
-            // Takes the "end" parameter in the query and tries to convert it to a number
-            end: params
-                .get("end")
-                .unwrap()
-                .parse::<usize>()
-                .map_err(Error::ParseError)?,
-        });
-    }
+pub fn extract_pagination(params: HashMap<String, String>) 
+    -> Result<Pagination, Error> {
+        // Could be improved in the future
+        if params.contains_key("start") && params.contains_key("end") {
+            return Ok(Pagination {
+                // Takes the "start" parameter in the query 
+                // and tries to convert it to a number
+                start: params
+                    .get("start")
+                    .unwrap()
+                    .parse::<usize>()
+                    .map_err(Error::ParseError)?,
+                // Takes the "end" parameter in the query 
+                // and tries to convert it to a number
+                end: params
+                    .get("end")
+                    .unwrap()
+                    .parse::<usize>()
+                    .map_err(Error::ParseError)?,
+            });
+        }
 
-    Err(Error::MissingParameters)
+        Err(Error::MissingParameters)
 }
