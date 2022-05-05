ae4039a35fda5f0898d1d499684c47704ebffbb4
Bastian Gruber <code@recv.online>
2022-05-05 07:08:51 +0200

    Update minimal-reqwest example

--- ch_01/minimal_reqwest/src/main.rs
+++ ch_01/minimal_reqwest/src/main.rs
@@ -1,7 +1,11 @@
+use std::collections::HashMap;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
         .await?;
-    println!("{:?}", resp);
+    println!("{:#?}", resp);
     Ok(())
 }
