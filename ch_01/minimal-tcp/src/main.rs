88321bf340ddd04432a23257bda0abe82b3ad8d2
Bastian Gruber <foreach@me.com>
2022-06-25 20:38:58 +0200

    Clippy

--- ch_01/minimal-tcp/src/main.rs
+++ ch_01/minimal-tcp/src/main.rs
@@ -10,10 +10,10 @@ fn main() {
 
 fn handle_stream(mut stream: TcpStream) { 
     let mut buffer = [0; 1024]; 
-    stream.read(&mut buffer).unwrap(); 
+    stream.read_exact(&mut buffer).unwrap(); 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let response = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).unwrap();
     stream.flush().unwrap(); 
 }
