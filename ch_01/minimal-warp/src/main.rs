c92b4e6dad49e78f68a8750b97944e0b100e5545
Bastian Gruber <foreach@me.com>
2022-07-22 09:51:03 -0400

    Update minimal_warp example

--- ch_01/minimal-warp/src/main.rs
+++ ch_01/minimal-warp/src/main.rs
@@ -3,7 +3,8 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     let hello = warp::path("hello")
-        .map(|| format!("Hello, World!"));
+        .and(warp::path::param())
+        .map(|name: String| format!("Hello, {}!", name));
 
     warp::serve(hello)
         .run(([127, 0, 0, 1], 1337))
